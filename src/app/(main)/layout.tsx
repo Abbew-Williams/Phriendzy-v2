d08d6844bd6dede3cc3c6179d74b2faf7ee0a8a2
Phriendzy Support <phriendzysup@gmail.com>
2026-03-14 16:53:16 +0000

    since the suggested for you is not there remove that container from ther

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,7 +2,6 @@ import { MainSidebar } from '@/components/main-sidebar';
 import { Sidebar, SidebarProvider } from '@/components/ui/sidebar';
 import { Toaster } from '@/components/ui/toaster';
 import { BottomNavBar } from '@/components/bottom-nav-bar';
-import { RightPanel } from '@/components/right-panel';
 
 export default function MainLayout({
   children,
@@ -11,7 +10,7 @@ export default function MainLayout({
 }) {
   return (
     <SidebarProvider>
-      <div className="md:grid md:grid-cols-[auto_1fr_320px] min-h-screen">
+      <div className="md:grid md:grid-cols-[auto_1fr] min-h-screen">
         {/* Sidebar for Desktop */}
         <div className="hidden md:flex">
             <Sidebar collapsible="icon" className="border-r border-border/50">
@@ -23,10 +22,6 @@ export default function MainLayout({
             {children}
         </main>
         
-        {/* Right Panel for Desktop */}
-        <aside className="hidden md:block border-l border-border/50">
-            <RightPanel />
-        </aside>
       </div>
       
       {/* Mobile nav and toaster */}
